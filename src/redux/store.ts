87478fb3c20f5d4c5b0585f0705d04a588044ee0
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-12-03 10:45:28 +0600

    Add Redux store configuration with auth, sales, and filters reducers

---
+++ src/redux/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit"
+import authReducer from "@/lib/slices/authSlice"
+import salesReducer from "@/lib/slices/salesSlice"
+import filtersReducer from "@/lib/slices/filtersSlice"
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    sales: salesReducer,
+    filters: filtersReducer,
+  },
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
